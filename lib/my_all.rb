4c423fbcf38ac5a6272b01090d39b14f76475453
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 19:05:27 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_all?(collection)
 count = 0
 
-array = []
+
   while count < collection.length
-  array  << yield(collection[count])
+    yield(collection[count])
   count += 1
 
   end
